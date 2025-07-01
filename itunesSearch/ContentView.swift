9355e883e9efc854ac9cc439227ece91af1c1228
scandinappia <scandinappia@gmail.com>
2025-07-01 09:59:59 +0200

    Initial Commit

---
+++ itunesSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  itunesSearch
+//
+//  Created by K P on 2025-07-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ itunesSearch/itunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  itunesSearchApp.swift
+//  itunesSearch
+//
+//  Created by K P on 2025-07-01.
+//
+
+import SwiftUI
+
+@main
+struct itunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
